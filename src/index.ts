a456ff214c6cdecedfbb741d2755a5d4f778c181
Vincent Acanfora <brodino96@gmail.com>
2025-07-16 21:54:15 +0200

    feat: removed config usage

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import Bot from "./bot"
-import { loadConfig } from "./utils/config"
 import { playCommand } from "./commands/music/play"
 import { pauseCommand } from "./commands/music/pause"
 import { resumeCommand } from "./commands/music/resume"
 import { skipCommand } from "./commands/music/skip"
 import { leaveCommand } from "./commands/music/leave"
 
-const config = loadConfig()
-const bot = new Bot(config)
+const bot = new Bot()
 
-bot.init([ playCommand, pauseCommand, resumeCommand, skipCommand, leaveCommand ])
+export const commands = [ playCommand, pauseCommand, resumeCommand, skipCommand, leaveCommand ]
+bot.init(commands)
