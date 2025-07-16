b00dd1243ffb26d351ca25eb121467fa435a81a8
Vincent Acanfora <brodino96@gmail.com>
2025-07-16 21:53:48 +0200

    feat: now uses the config export

--- src/bot.ts
+++ src/bot.ts
@@ -1,21 +1,19 @@
 import { Client } from "discord.js"
-import { Config } from "./utils/config"
+import { config } from "./utils/config"
 import { tryCatch } from "typecatch"
 import { Command, CommandManager } from "./commands/commands"
 
 export default class Bot {
     public client: Client
-    public config: Config
 
-    constructor(config: Config) {
-        this.config = config
+    constructor() {
         this.client = new Client({
             intents: [ "Guilds", "GuildMessages", "GuildVoiceStates", "MessageContent" ]
         })
     }
 
     public async init(commands: Command[]): Promise<void> {
-        const { error: loginError } = await tryCatch(this.client.login(this.config.token))
+        const { error: loginError } = await tryCatch(this.client.login(config.token))
         if (loginError) {
             console.error(`Failed to login: ${loginError}`)
             return process.exit(0)
